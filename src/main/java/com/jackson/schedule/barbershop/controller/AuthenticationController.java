19a6fb9f3b31d46c72b2256230054fb5e5fc5339
jacksuh <jacksonsdss@gmail.com>
2023-04-06 09:42:58 -0300

    Authentication

--- src/main/java/com/jackson/schedule/barbershop/controller/AuthenticationController.java
+++ src/main/java/com/jackson/schedule/barbershop/controller/AuthenticationController.java
@@ -1,5 +1,4 @@
 package com.jackson.schedule.barbershop.controller;
-
 import com.jackson.schedule.barbershop.dto.user.TokenJwtDto;
 import com.jackson.schedule.barbershop.dto.user.UserAuthentication;
 import com.jackson.schedule.barbershop.infra.springsecurity.TokenService;
@@ -12,10 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("login")
-public class AutenticacaoController {
+public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager manager;
@@ -24,7 +22,7 @@ public class AutenticacaoController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody UserAuthentication dto){
+    public ResponseEntity login(@RequestBody UserAuthentication dto){
         var authenticationToken = new UsernamePasswordAuthenticationToken(dto.getLogin(), dto.getPassword());
         var authentication = manager.authenticate(authenticationToken);
 
