7fb1b9f9e04f515931b22a2fc3414a7c98a751f2
jacksuh <jacksonsdss@gmail.com>
2023-04-06 09:34:40 -0300

    Authentication

--- src/main/java/com/jackson/schedule/barbershop/controller/AuthenticationController.java
+++ src/main/java/com/jackson/schedule/barbershop/controller/AuthenticationController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("login")
 public class AuthenticationController {
-
     @Autowired
     private AuthenticationManager manager;
 
