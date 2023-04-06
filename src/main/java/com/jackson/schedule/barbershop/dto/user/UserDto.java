b5881e1070ac164b09a11684767ba815f7b6facf
jacksuh <jacksonsdss@gmail.com>
2023-04-06 09:38:02 -0300

    Authentication

--- src/main/java/com/jackson/schedule/barbershop/dto/user/UserDto.java
+++ src/main/java/com/jackson/schedule/barbershop/dto/user/UserDto.java
@@ -2,12 +2,10 @@ package com.jackson.schedule.barbershop.dto.user;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 @Setter
 @Getter
 @NoArgsConstructor
 public class UserDto {
-
     private String login;
     private String password;
 
