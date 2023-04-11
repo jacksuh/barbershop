e55f34f145273836d396899073428f8c5d720fac
jacksuh <jacksonsdss@gmail.com>
2023-04-11 13:28:40 -0300

    update: Doc

--- src/main/java/com/jackson/schedule/barbershop/infra/springdoc/SpingDocConfigurations.java
+++ src/main/java/com/jackson/schedule/barbershop/infra/springdoc/SpingDocConfigurations.java
@@ -1,8 +1,10 @@
 package com.jackson.schedule.barbershop.infra.springdoc;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
@@ -10,7 +12,11 @@ public class SpingDocConfigurations {
 
     @Bean
     public OpenAPI customOpenAPI(){
-        return new OpenAPI().info(new Info()
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
                 .title("BarberShop")
                 .description("API Rest criada para agendamento de horario/serviço na barbearia")
                 .contact(new Contact()
